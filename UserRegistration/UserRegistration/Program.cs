08b1d59e5a39727fc08f1a015f91903d32a77810
Harinilekkala <harini.lekkala1997@gmail.com>
2022-06-05 15:10:47 +0530

    [Harini]: Check First Name

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+UserRegistration.RegularExpression regex = new UserRegistration.RegularExpression();
+Console.WriteLine("1 - Enter First Name");
+//Console.WriteLine("2 - Enter Last Name");
+int select = Convert.ToInt32(Console.ReadLine());
+switch (select)
+{
+    case 1:
+        Console.WriteLine("Enter First Name");
+        string fName = Console.ReadLine();
+        regex.name(fName);
+        break;
+
+}
